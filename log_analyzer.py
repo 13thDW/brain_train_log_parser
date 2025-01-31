c3a0d423e7a38fadb93c8663698530a0120c4135
13thDW <13th.drunken.whaler@gmail.com>
2025-01-31 06:05:00 -0500

    added regex, used func, now have func for future search system, new func for counting by params

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,36 +1,80 @@
+import re
+from datetime import datetime
+
+def logs_parser(file: str) -> list:
+    log_data = []
+
+    with open(file, 'r', encoding='utf-8') as log:
+
+        regex = r'(\d{4} \w{3} \d{2} \d{2}:\d{2}:\d{2}) (\w+)\s?(\w*): (\S+) (.*)'
+
+        for line in log:
+            match = re.match(regex, line)
+
+            if match:
+                date = datetime.strptime(match.group(1), "%Y %b %d %H:%M:%S")
+                event = match.group(2)
+                source = match.group(3) if match.group(3) else "Unknown"
+                code = match.group(4)
+                content = match.group(5)
+            else:
+                print(f'No match found for: {line}')
+            
+            log_data.append({
+                    'date' : date,
+                    'event' : event,
+                    'source' : source,
+                    'code' : code,
+                    'content' : content
+                    })
+
+    return log_data
+
+
+def logs_sorter(logs: list) -> list:
+    sorted_log = sorted(logs, key=lambda item: item['date'])
+
+    with open('sorted_logs.txt', 'w', encoding='utf-8') as sl:
+        for line in sorted_log:
+            sl.write(f'{line['date']} {line['event']} {line['source']} : {line['code']} {line['content']}\n')
+        
+    return sorted_log    
+
+
+def unique_parameters_set(logs: list, parameter: str) -> list:
+    unique_parameters = list(set(line[parameter] for line in logs))
+    return unique_parameters
+
+
+def logs_counter(logs: list, parameters: list, parameter: str) -> dict:
+    uniq_params_counter = {}
+    for param in parameters:
+        cntr = 0
+        for line in logs:
+            if line[parameter] == param:
+                cntr += 1
+                uniq_params_counter[param] = cntr
+
+    return uniq_params_counter
+
+
+def split_logs_by_event(logs: list, events: list):
+    for event in events:
+        with open(f'{event}.txt', 'a', encoding='utf-8') as evnt:
+            for line in logs:
+                if line['event'] == event:
+                    evnt.write(f'{line['date']} {line['event']} {line['source']} : {line['code']} {line['content']}\n')
+    
 
 try:
-    lst = []
-    code_dict = {}
-    error_lines = []
-    with open('log.txt', 'r', encoding='utf-8') as log:
-        data = list(log)
 
-        for i in data:
-            key_value = [' '.join(i.split(" ")[:2]), ' '.join(i.split(" ")[2:])]
-            
-            if code_dict.get(key_value[1].split(" ")[0]) == None:
-                code_dict[key_value[1].split(" ")[0]] = 1
-            else:
-                code_dict[key_value[1].split(" ")[0]] += 1
-            
-            if key_value[1].split(" ")[0] == "ERROR":
-                error_lines.append(i)
-
-            lst.append(key_value)
-    
-    with open('error.txt', 'w', encoding='utf-8') as error:
-        error.writelines(error_lines)
-
-    
-    print(code_dict)
-
-    lst = sorted(lst, key=lambda value: value[0])
-
-    with open('log.txt', 'w', encoding='utf-8') as log:
-        for i in lst:
-            log.write(' '.join(i))
-
+    file = 'messages_SYS.log'
+    log_data = logs_parser(file)
+    sorted_log = logs_sorter(log_data)
+    unique_events = unique_parameters_set(sorted_log, 'event')
+    uniq_events_counter = logs_counter(sorted_log, unique_events, 'event')
+    split_logs_by_event(sorted_log, unique_events)
+    print(f"Earliest record: {sorted_log[0]['date']};\nLatest record: {sorted_log[-1]['date']};\n{uniq_events_counter}\nList of unique events: {unique_parameters_set(sorted_log, 'content')}")
 
 except FileNotFoundError:
-    print("No such file or directory")
+    print('No such file or directory')
